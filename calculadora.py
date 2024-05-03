ada4427a8887a7e9d45867dc395cf58e681e28b6
sasyerag <164093190+sasyerag@users.noreply.github.com>
2024-05-03 07:36:44 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def soma(a, b):
+    return a + b
+
+if_name_=="_main_":
+  num1 = float(input("digite o primeiro número: "))
+  num2 = float(input("digite o segundo número: ")
+  resultado = soma(num1, num2)
+  peint(f"resultado: {resultado}")
